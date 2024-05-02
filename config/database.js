c08ef07ea881ca10fa6919f099ae94735c375929
jnsprnw <6gqrbj@posteo.org>
2024-05-02 15:59:52 +0200

    Correct database (local)

--- config/database.js
+++ config/database.js
@@ -1,11 +1,28 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: "postgres",
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      // connectionString: env('DATABASE_URL'),
+      host: env("DATABASE_HOST", "localhost"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "metalab-v4"),
+      user: env("DATABASE_USERNAME", "jonas"),
+      password: env("DATABASE_PASSWORD", "WBKFxw4Zo@@.NYVkBBfBdM!AKDpk2z"),
+      ssl: env.bool("DATABASE_SSL", false) && {
+        key: env("DATABASE_SSL_KEY", undefined),
+        cert: env("DATABASE_SSL_CERT", undefined),
+        ca: env("DATABASE_SSL_CA", undefined),
+        capath: env("DATABASE_SSL_CAPATH", undefined),
+        cipher: env("DATABASE_SSL_CIPHER", undefined),
+        rejectUnauthorized: env.bool("DATABASE_SSL_REJECT_UNAUTHORIZED", true),
+      },
+      schema: env("DATABASE_SCHEMA", "public"),
     },
-    useNullAsDefault: true,
+    pool: {
+      min: env.int("DATABASE_POOL_MIN", 2),
+      max: env.int("DATABASE_POOL_MAX", 10),
+    },
   },
 });
