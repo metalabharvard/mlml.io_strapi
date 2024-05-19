0b6b1936952040023e74abb56e3bb9c7d0140b1a
jnsprnw <6gqrbj@posteo.org>
2024-05-19 22:35:05 +0200

    Add Heroku configuration

--- config/database.js
+++ config/database.js
@@ -1,15 +1,12 @@
-const path = require("path");
-
 module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      // connectionString: env('DATABASE_URL'),
-      host: env("DATABASE_HOST", "localhost"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "metalab-v4"),
-      user: env("DATABASE_USERNAME", "jonas"),
-      password: env("DATABASE_PASSWORD", "WBKFxw4Zo@@.NYVkBBfBdM!AKDpk2z"),
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT"),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
       ssl: env.bool("DATABASE_SSL", false) && {
         key: env("DATABASE_SSL_KEY", undefined),
         cert: env("DATABASE_SSL_CERT", undefined),
